d72ed49da8fe8cd247afdefb0301bc33554a5b65
Zhongke Chen <github@ch3n2k.com>
2023-02-20 22:12:54 -0800

    remove dependency on urllib3

--- rslsync/api.py
+++ rslsync/api.py
@@ -5,18 +5,23 @@ import logging
 import re
 import time
 from urllib.parse import urljoin
-
-import urllib3
+from base64 import b64encode
+import urllib.request
 
 
 class RslClient:
     def __init__(self, url, username, password, timeout=30):
         self.url = urljoin(url, "gui/")
-        self.auth_headers = urllib3.make_headers(basic_auth=username + ":" + password)
         self.timeout = timeout
         self.auth_cookie = None
         self.token = None
-        self.authenticate()
+        self.auth_headers = self._basic_auth_headers(username, password)
+        self.refresh_token()
+
+    @staticmethod
+    def _basic_auth_headers(username, password):
+        token = b64encode(f"{username}:{password}".encode('utf-8')).decode("ascii")
+        return {"Authorization": "Basic " + token}
 
     @staticmethod
     def _current_ts():
@@ -26,13 +31,14 @@ class RslClient:
     def _convert_parameter(value):
         return str(value).lower() if isinstance(value, bool) else value
 
-    def authenticate(self):
+    def refresh_token(self):
         url = urljoin(self.url, "token.html")
-        fields = {"t": self._current_ts()}
-        response = urllib3.PoolManager().request("GET", url, headers=self.auth_headers, fields=fields, timeout=self.timeout)
-        self.auth_cookie = response.headers["Set-Cookie"].split(";")[0]
+        fields = urllib.parse.urlencode({"t": self._current_ts()})
+        request = urllib.request.Request(url + "?" + fields, headers=self.auth_headers)
+        response = urllib.request.urlopen(request, timeout=self.timeout)
+        self.auth_cookie = response.getheader("Set-Cookie").split(";")[0]
         pattern = r"<div id='token' .*>(?P<token>[\w-]+)</div>"
-        m = re.search(pattern, response.data.decode("utf-8"))
+        m = re.search(pattern, response.read().decode("utf-8"))
         if m:
             self.token = m.group('token')
         else:
@@ -42,9 +48,10 @@ class RslClient:
         logging.debug("get: ", action, params)
         headers = {"Cookie": self.auth_cookie, **self.auth_headers}
         params = {key: self._convert_parameter(value) for key, value in params.items()}
-        fields = {"token": self.token, 'action': action, **params, "t": self._current_ts()}
-        response = urllib3.PoolManager().request("GET", self.url, headers=headers, fields=fields, timeout=self.timeout)
-        j = json.loads(response.data.decode('utf-8'))
+        fields = urllib.parse.urlencode({"token": self.token, 'action': action, **params, "t": self._current_ts()})
+        request = urllib.request.Request(self.url + "?" + fields, headers=headers)
+        response = urllib.request.urlopen(request, timeout=self.timeout)
+        j = json.loads(response.read().decode('utf-8'))
         if verify and j.get("status") != 200:
             logging.error(j)
         return j
